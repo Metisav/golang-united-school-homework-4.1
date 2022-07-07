9840b7625e9e4a85d77527fccab8561844807b58
artem <metisav@gmail.com>
2022-07-07 22:01:46 +0500

    решение

---
+++ go.mod
@@ -0,0 +1,3 @@
+module epam-go/golang-united-school-homework-4.1
+
+go 1.18
--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,12 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
-	return output
+
+	in_runes := []rune(input)
+	out_runes := make([]rune, len(input))
+	for k, v := range in_runes {
+		out_runes[len(in_runes)-k-1] = v
+	}
+	output = string(out_runes)
+	return
 }
